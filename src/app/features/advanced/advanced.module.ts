427fc7a0a0fb2f3689f9fdbe4e2431e4ff1f63d2
tzion <tzion800@gmail.com>
2025-10-07 15:53:40 +0300

    adding the actions,reducer,selector pages

--- src/app/features/advanced/advanced.module.ts
+++ src/app/features/advanced/advanced.module.ts
@@ -11,10 +11,17 @@ import { SelectComponent } from './pages/settings-page/components/select/select.
 import { TextComponent } from './pages/settings-page/components/text/text.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentStateComponent } from './pages/state-management-page/components/component-state/component-state.component'; 
-import { StateManagementComponent } from './pages/state-management-page/state-management';
+import { StateManagementComponent } from './pages/state-management-page/state-management.component';
 import { RouterModule } from '@angular/router';
 import { RxjsVsSignalsComponent } from './pages/state-management-page/components/rxjs-vs-signals/rxjs-vs-signals.component';
+import { NgrxComponent} from './pages/ngrx-page/ngrx.component';
 import { GlobalPatternComponent } from './pages/state-management-page/components/global-pattern/global-pattern.component';
+import { NgrxActionsComponent } from './pages/ngrx-page/components/ngrx-actions/ngrx-actions/ngrx-actions.component';
+import { CounterDemoComponent } from './pages/ngrx-page/components/counter-demo/counter-demo.component';
+import { CounterDemoSelectorsComponent } from './pages/ngrx-page/components/counter-demo-selector/counter-demo-selectors.component';
+import { NgrxReducersComponent } from './pages/ngrx-page/components/ngrx-reducers/ngrx-reducers.component';
+
+import { NgrxSelectorsComponent } from './pages/ngrx-page/components/ngrx-selectors/ngrx-selectors.component';
 @NgModule({
   declarations: [
     AdvancedHomeComponent,
@@ -26,8 +33,14 @@ import { GlobalPatternComponent } from './pages/state-management-page/components
     SelectComponent,
     ComponentStateComponent,
     StateManagementComponent,
+    NgrxComponent,
     RxjsVsSignalsComponent,
     GlobalPatternComponent,
+    NgrxActionsComponent,
+    CounterDemoComponent,
+    CounterDemoSelectorsComponent,
+    NgrxReducersComponent,
+    NgrxSelectorsComponent,
   ],
   imports: [
     CommonModule,
